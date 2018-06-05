637c58ff0dac13cd232d1065ed3ab9560ae91e5c
Chinz254 <kennedymuchina@gmail.com>
2018-06-05 11:46:56 +0300

    ...

--- portfolioApp/portfolioApp/settings.py
+++ portfolioApp/portfolioApp/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
