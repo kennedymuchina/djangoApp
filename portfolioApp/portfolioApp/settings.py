0afe0af253d581d654d23f575e281eccac976c4e
Chinz254 <kennedymuchina@gmail.com>
2018-06-05 11:50:26 +0300

    ...

--- portfolioApp/portfolioApp/settings.py
+++ portfolioApp/portfolioApp/settings.py
@@ -123,7 +123,12 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
